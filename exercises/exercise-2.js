8289a54800f3d8fcbb24714e21426baf74c042d5
David Lichtsteiner <david@lichtsteiner.li>
2019-11-01 17:00:35 +0100

    Solution Exercise 2

--- exercises/exercise-2.js
+++ exercises/exercise-2.js
@@ -1,7 +1,9 @@
 // Exercise 2: Write a function that returns an array with the email addresses of all employees using a higher order function.
 
 function getEmails(employees) {
-
+    return employees.map(employee => {
+        return employee.email;
+    });
 }
 
 module.exports = getEmails;
