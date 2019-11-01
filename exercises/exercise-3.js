14b8ff78f8bfc1692e0f59bc22fb52893a0f6a1d
David Lichtsteiner <david@lichtsteiner.li>
2019-11-01 17:01:26 +0100

    Solution Exercise 3

--- exercises/exercise-3.js
+++ exercises/exercise-3.js
@@ -1,7 +1,9 @@
 // Exercise 3: Write a function that returns the sales employees using a higher order function.
 
 function getSalesEmployees(employees) {
-
+    return employees.filter(employee => {
+        return employee.department === 'Sales';
+    });
 }
 
 module.exports = getSalesEmployees;
