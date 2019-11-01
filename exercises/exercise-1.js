26d88300caf1c00b77e07907888d59776e0eeb33
David Lichtsteiner <david@lichtsteiner.li>
2019-11-01 16:56:24 +0100

    first commit

---
+++ exercises/exercise-1.js
@@ -0,0 +1,7 @@
+// Exercise 1: Write a function that returns an array with the email addresses of all employees:
+
+function getEmails(employees) {
+
+}
+
+module.exports = getEmails;
---
+++ exercises/exercise-3.js
@@ -0,0 +1,7 @@
+// Exercise 3: Write a function that returns the sales employees using a higher order function.
+
+function getSalesEmployees(employees) {
+
+}
+
+module.exports = getSalesEmployees;
