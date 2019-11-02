039c9485283ae222b5aa819058043a4265b3646a
David Lichtsteiner <david@lichtsteiner.li>
2019-11-02 12:00:30 +0100

    Solution exercise 1

--- exercises/exercise-1.js
+++ exercises/exercise-1.js
@@ -1,7 +1,13 @@
 // Exercise 1: Write a function that returns an array with the email addresses of all employees using a common loop:
 
 function getEmails(employees) {
+    const emails = [];
 
+    for (i = 0; i < employees.length; i++) {
+        const employee = employees[i];
+        emails.push(employee.email);
+    }
+    return emails;
 }
 
 module.exports = getEmails;
