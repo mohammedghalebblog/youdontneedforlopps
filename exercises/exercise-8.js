130362e4f6c344571eb3596dbd4ddd261516fe18
David Lichtsteiner <david@lichtsteiner.li>
2019-11-07 20:36:15 +0100

    Solution Exercise 8

--- exercises/exercise-8.js
+++ exercises/exercise-8.js
@@ -1,7 +1,7 @@
 // Exercise 8: Write a function that sums all numbers of an array.
 
 function sum(numbers) {
-
+    return numbers.reduce((total, number) => total + number, 0);
 }
 
 module.exports = sum;
