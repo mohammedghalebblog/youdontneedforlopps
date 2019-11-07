26a9bd9a034005dcf20042f30e99c56d7b5e9c2a
David Lichtsteiner <david@lichtsteiner.li>
2019-11-07 20:23:05 +0100

    Solution Exercise 4

--- exercises/exercise-4.js
+++ exercises/exercise-4.js
@@ -1,25 +1,25 @@
 // Exercise 4: Rewrite this function using a higher order functions.
 
 function getBestRevenuesTotal(employees) {
-    let totalRevenue = 0;
     const minSales = 75000;
 
-    for (let employee of employees) {
-        if(employee.department === 'Sales') {
-            const customers = employee.customers;
-            for (let customer of customers) {
-                let customerTotalSales = 0;
-                for  (let order of customer.orders) {
-                    customerTotalSales += order.total;
-                }
-                if (customerTotalSales >= minSales) {
-                    totalRevenue += customerTotalSales
-                }
-            }
-        }
-    }
-
-    return totalRevenue;
+    return employees
+        .filter(employee => {
+            return employee.department === 'Sales';
+        })
+        .map((salesEmployees) => {
+            return salesEmployees.customers;
+        })
+        .flat(1)
+        .map(customer => {
+            return customer.orders.reduce((acc, order) => {
+                return acc + order.total;
+            }, 0);
+        })
+        .filter(orderTotal => orderTotal >= minSales)
+        .reduce((acc, orderTotal) => {
+            return acc + orderTotal;
+        }, 0);
 }
 
 module.exports = getBestRevenuesTotal;
