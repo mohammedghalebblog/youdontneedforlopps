35f7aa083b3bdcf3d98c85b154550308ac3d570a
David Lichtsteiner <david@lichtsteiner.li>
2019-11-07 20:31:33 +0100

    Solution Exercise 6

--- exercises/exercise-6.js
+++ exercises/exercise-6.js
@@ -1,7 +1,7 @@
 // Exercise 6: Write a function that turn each day of an array to its weekday.
 
 function weekDays(dates) {
-
+    return dates.map(date => date.toLocaleDateString('en-US', {weekday: 'long'}));
 }
 
 module.exports = weekDays;
