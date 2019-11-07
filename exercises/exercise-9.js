876db2de214fcb5503e3343c846d654207308d33
David Lichtsteiner <david@lichtsteiner.li>
2019-11-07 20:36:35 +0100

    Solution Exercise 9

--- exercises/exercise-9.js
+++ exercises/exercise-9.js
@@ -1,7 +1,9 @@
 // Exercise 9: Calculate the total value of the Shopping Cart.
 
 function getShoppingCartTotal(shoppingCart) {
-
+    return shoppingCart
+        .map(item => item.quantity * item.unitPrice)
+        .reduce((totalPrice, itemPrice) => totalPrice + itemPrice, 0);
 }
 
 module.exports = getShoppingCartTotal;
