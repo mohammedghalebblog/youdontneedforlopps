7c8a73a7f0e54c8eaef792b53a881f33fa6ae595
David Lichtsteiner <david@lichtsteiner.li>
2019-11-07 20:49:14 +0100

    Solution Exercise 10

--- exercises/exercise-10.js
+++ exercises/exercise-10.js
@@ -1,7 +1,13 @@
 // Exercise 10: Write a function that return the number of employees in each Department.
 
 function employeesPerDepartment(employees) {
+    return employees.reduce((employeesPerDepartment, employee) => {
+        employeesPerDepartment.hasOwnProperty(employee.department) ?
+            employeesPerDepartment[employee.department] = ++employeesPerDepartment[employee.department] :
+            employeesPerDepartment[employee.department] = 1;
 
+        return employeesPerDepartment;
+    }, {});
 }
 
 module.exports = employeesPerDepartment;
