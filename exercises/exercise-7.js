a4424ff1f5a8058c40a5d641cf4a1fa70996c3fb
David Lichtsteiner <david@lichtsteiner.li>
2019-11-07 20:32:59 +0100

    Solution Exercise 7

--- exercises/exercise-7.js
+++ exercises/exercise-7.js
@@ -1,7 +1,7 @@
 // Exercise 7: Write a function that removes odd numbers of an array
 
 function removeOddNumbers(numbers) {
-
+    return numbers.filter(number => !(number%2));
 }
 
 module.exports = removeOddNumbers;
