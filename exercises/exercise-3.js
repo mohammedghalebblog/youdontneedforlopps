9c9dbae0de4be025690a479d4bed26c3da9d7fdc
Mohammed Ghaleb <ghaleb.ch@gmail.com>
2020-05-19 10:17:35 +0200

    Update exercise-3.js

--- exercises/exercise-3.js
+++ exercises/exercise-3.js
@@ -1,7 +1,5 @@
 // Exercise 3: Write a function that returns the sales employees using a higher order function.
 
-function getSalesEmployees(employees) {
-
-}
+function getSalesEmployees(employees) {}
 
 module.exports = getSalesEmployees;
